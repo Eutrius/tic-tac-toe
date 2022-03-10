2b22e94834f7bf34410cd729a8090bc4f4f64198
Eutrius <eutrius@gmail.com>
2022-03-10 20:11:42 +0100

    gameBoard functions done

--- main.js
+++ main.js
@@ -1,10 +1,3 @@
-let container = document.getElementById("container");
-const chooseOpponent = document.getElementsByClassName("enemies");
-
-chooseOpponent.forEach(element => {
-    element.addEventListener("click",OpponentChosen);
-});
-container.addEventListener("click", clicked);
 
 
 function clicked(e) {
@@ -18,46 +11,98 @@ function OpponentChosen(e) {
     } 
 }
 
-const gameManager = (() => {
-
-
-})();
-
-
 const gameBoard = (() => {
     let boardCells = [];
-    let gameBoardDiv = document.getElementById("board");
+    let element = document.getElementById("board");
 
     const init = () => {
         for (let i = 0; i < 9; i++) {
-            cell = generateCell(i);
-            boardCells.push(cell);
-            gameBoardDiv.appendChild(cell);
+            let tempCell = cell(i);
+            boardCells.push(tempCell);
+            element.appendChild(tempCell.element);
         }
     }
 
     const reset = () => {
         for (let cell of boardCells) {
-            cell.textContent = '';
+            cell.reset();
         }
     }
-    
 
-    const generateCell = (id) => {
-        let tempDiv = document.createElement("div");
-        tempDiv.id = `${id}`;
-        tempDiv.className = "cell";
-        return tempDiv;
+    const markCell = (index, mark) => {
+        boardCells[index].getMark(mark);
+        console.log(boardCells[index])
     }
 
-    return {boardCells, init, reset};
+    const isMarked = (index) => {
+        return boardCells[index].isMarked();
+    }
+
+    const cell = (id) => {
+        let marked = false;
+        let element = document.createElement("div");
+        element.id = `${id}`;
+        element.className = "cell";
+
+        const getMark = (mark) => {
+            marked = true;
+            element.textContent = mark;
+        }
+
+        const reset = () => {
+            marked = false;
+            element.textContent = '';
+        }
+
+        const isMarked = () => {
+            return marked;
+        }
+        return {id,element,getMark,reset,isMarked};
+    } 
+    
+
+
+
+    return {element, init, reset,markCell,isMarked};
 })();
+
 
 
 
 const player = (mark) => {
     let markedCell = [];
+
+    const pushCell = (cellIndex) => {
+        markedCell.push(cellIndex);
+    }
+
+    return {markedCell,mark,pushCell};
 }
 
 
-gameBoard.init();
+
+const gameManager = (() => {
+    gameBoard.init();
+    const playerX = player("X");
+    const playerO = player("O");
+
+    let currPlayer = playerX;
+
+
+    const play = (e) => {
+        if (!e.target.classList.contains("cell")) return;
+        let index = parseInt(e.target.id);
+        if (gameBoard.isMarked(index)) return;
+        gameBoard.markCell(index,currPlayer.mark);
+        currPlayer.pushCell(index);
+        console.log(currPlayer.markedCell);
+        playerChange();
+    }
+
+    const playerChange = () => {
+        currPlayer = currPlayer == playerX ? playerO : playerX;
+    }
+
+
+    gameBoard.element.addEventListener("click", play);
+})();
